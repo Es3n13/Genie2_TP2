a0e67dc182f85d9e6fe2cd65c515a44c9d0a02b4
Es3n13 <emile.guy.santerre@gmail.com>
2025-11-09 22:03:06 -0500

    Add files via upload

---
+++ Genie2_TP2_V2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Genie2_TP2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
